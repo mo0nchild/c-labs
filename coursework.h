8b1cd2cd9f54d3f4c22bd1ecc6cef984a84b06b9
Danil <68150308+mo0nchild@users.noreply.github.com>
2021-10-19 00:36:52 +0300

    Add files via upload

--- coursework.h
+++ coursework.h
@@ -14,93 +14,99 @@
 #define HORIZ 1
 #define VERTIC 7
 
+#define CLEAR_FRAME(void) system("cls")
+#define bool _Bool
+
 #define LIFE_COUNT 3
 
-const char line[] = "\n -----\t -----\t -----\t -----\t -----\t -----\n";
+const char FIELD_LINE[] = "\n -----\t -----\t -----\t -----\t -----\t -----\n";
 
-enum KEY_CODE
+typedef enum 
 {
     KEY_A = 97,
     KEY_D = 100,
     KEY_S = 115,
     KEY_W = 119,
     KEY_SPACE = 32
-};
+} KEY_CODE;
+typedef enum { RUNNING, WIN, LOSE } GAME_STATE;
+typedef enum { MAINMENU, SETTINGS, GAME, EDITOR } SCENES;
+typedef enum { FALSE_T, TRUE_T, ACCEPT } ACTIONS;
 
-typedef enum state {WIN = 1, LOSE = 2, RUNNING = 0} GAME_STATE;
-
-typedef struct CELL
+typedef struct
 {
-    int free_cells_x, free_cells_y;
+    int free_cells_x;
+    int free_cells_y;
     int check_value;
 } CELL;
-
-typedef struct POSITION { int x, y; } POS;
-
-POS set_POS(int x, int y) // ГЄГ®Г­Г±ГІГ°ГіГЄГІГ®Г° Г¤Г«Гї Г±ГІГ°ГіГЄГІГіГ°Г» POSITION
+CELL set_cell(int check, int free)
 {
-    POS pos = { .x = x, .y = y };
+    CELL cell = { free, free, check };
+    return cell;
+}
+
+typedef struct 
+{ 
+    int x; 
+    int y;
+} POSITION;
+POSITION set_position(int x, int y) // конструктор для структуры POSITION
+{
+    POSITION pos = { .x = x, .y = y };
     return pos;
 }
 
-CELL field[7][7] = 
-{
-    { {7,7,0},{7,7,8},{7,7,0},{7,7,0},{7,7,0},{7,7,0},{7,7,9} },
-    { {7,7,5},{7,7,3},{7,7,0},{7,7,0},{7,7,0},{7,7,3},{7,7,4} },
-    { {7,7,0},{7,7,0},{7,7,0},{7,7,0},{7,7,4},{7,7,0},{7,7,0} },
-    { {7,7,0},{7,7,4},{7,7,0},{7,7,0},{7,7,0},{7,7,6},{7,7,0} },
-    { {7,7,0},{7,7,0},{7,7,5},{7,7,0},{7,7,0},{7,7,0},{7,7,0} },
-    { {7,7,8},{7,7,9},{7,7,0},{7,7,0},{7,7,0},{7,7,10},{7,7,9} },
-    { {7,7,2},{7,7,0},{7,7,0},{7,7,0},{7,7,0},{7,7,5},{7,7,0} }
-};
+CELL field[7][7];
 
-typedef _Bool action_method (POS* p, HANDLE* c, int *l, _Bool* e);
-
+int* read_field_from_file(const char* filename, int field_size);
 void set_line(int dir, int* last, int i, int* cell);
-_Bool get_keyboard_input(POS* pos);
-_Bool check_axis(POS pos);
-GAME_STATE draw_field(POS pos, HANDLE* c);
+
+ACTIONS get_keyboard_input(POSITION* pos, POSITION max);
+bool check_axis(POSITION pos);
+
+GAME_STATE draw_field(POSITION pos);
 
 void run_app(void)
 {
     int life_counter = LIFE_COUNT;
-    _Bool game_end = FALSE;
+    bool game_end = FALSE;
 
-    HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
-    POS cursor_pos = set_POS(0, 0);
+    POSITION cursor_pos = set_position(0, 0);
+
+    int* values = read_field_from_file("data.txt", 49);
+
 start:
     for (int i = 0; i < 7; i++)
     {
-        for (int k = 0; k < 7; k++) 
-        {
-            field[i][k].free_cells_x = 7;
-            field[i][k].free_cells_y = 7;
-            field[i][k].check_value = (field[i][k].check_value == BLACK_CELL ?
-                WHITE_CELL : field[i][k].check_value);
-        }
+        for (int k = 0; k < 7; k++) field[i][k] = set_cell(values[(i * 7 + k)], 7);
     }
 
+    ACTIONS input = TRUE_T;
     while (game_end != TRUE)
     {
-        system("cls");
-        printf("\nWASD: to control\tSPACE: to accept\nLIFE: %d\n", life_counter);
-
-        switch (draw_field(cursor_pos, console))
+        switch (input)
         {
+        case ACCEPT: check_axis(cursor_pos);
+        case TRUE_T:
+            CLEAR_FRAME();
+            printf("\nWASD: to control\tSPACE: to accept\nLIFE: %d\n", life_counter);
 
-        case WIN: game_end = TRUE; 
-        case LOSE:
-            if (--life_counter <= 0) game_end = TRUE;
-            check_axis(cursor_pos);
-            continue;
-        case RUNNING:
-        default: break;
+            switch (draw_field(cursor_pos))
+            {
+
+            case WIN: game_end = TRUE;
+            case LOSE:
+                if (--life_counter <= 0) game_end = TRUE;
+                continue;
+            case RUNNING:
+            default: break;
+            }
+        case FALSE_T:break;
         }
-
-        while (1) if (get_keyboard_input(&cursor_pos)) break;
+        input = get_keyboard_input(&cursor_pos, set_position(7, 7));
     }
 
-    system("cls");
+    CLEAR_FRAME();
     printf("\n\n\n\n\n\t\t%10s\n\n\n\n", (life_counter ?
         "YOU WIN" : "YOU LOSE"));
 
@@ -108,44 +114,53 @@ start:
     life_counter = LIFE_COUNT;
     game_end = FALSE;
     goto start;
-    
 
 }
 
-_Bool get_keyboard_input(POS *pos) 
+int* read_field_from_file(const char* filename, int field_size)
 {
-    _Bool state = TRUE;
+    int* result = (int*)malloc(sizeof(int) * field_size), n = 0;
+    FILE* file = fopen(filename, "rt");
+
+    while (!feof(file))fscanf(file, "%d", &result[n++]);
+
+    return result;
+}
+
+ACTIONS get_keyboard_input(POSITION *pos, POSITION max) // стрелочное управление WASD & SPACE
+{
+    ACTIONS state = TRUE_T;
     switch (getch())
     {
     case KEY_A: pos->x = (pos->x - 1 < 0 ? 0 : pos->x - 1); break;
-    case KEY_D: pos->x = (pos->x + 1 > 6 ? 6 : pos->x + 1); break;
+    case KEY_D: 
+        pos->x = (pos->x + 1 > max.x-1 ? max.x - 1 : pos->x + 1);
+        break;
     case KEY_W: pos->y = (pos->y - 1 < 0 ? 0 : pos->y - 1); break;
-    case KEY_S: pos->y = (pos->y + 1 > 6 ? 6 : pos->y + 1); break;
-    case KEY_SPACE: check_axis(*pos); break;
-    default: state = FALSE; break;
+    case KEY_S: 
+        pos->y = (pos->y + 1 > max.y - 1 ? max.y - 1 : pos->y + 1);
+        break;
+    case KEY_SPACE: state = ACCEPT; break;
+    default: state = FALSE_T; break;
     }
     return state;
 }
 
 
-void set_line(int dir, int* last, int i, int * cell)// ГЇГҐГ°ГҐГЇГЁГ±Гј Г§Г­Г Г·ГҐГ­ГЁГ© ГўГ»ГЎГ°Г Г­Г­Г®Г© Г®Г±ГЁ
+void set_line(int dir, int* last, int i, int * cell)// перезапись значений свобоных клеток выбранной оси
 {
     int value = i - (*last);
-    for (; (*last) < i; (*last)++)
-    {
-        *cell = value;
-        cell += (3 * dir);
-    }
+    for (; (*last) < i; (*last)++, cell += (3 * dir)) *cell = value;
     (*last)++;
 }
 
-_Bool check_axis(POS pos)//ГЇГ°Г®ГўГҐГ°ГЄГ  ГЇГ® Г¤ГўГіГ¬ Г®Г±ГїГ¬ Г­Г  ГЇГҐГ°ГҐГ±ГҐГ·ГҐГ­ГЁГЁ x and y
+bool check_axis(POSITION pos)//проверка по двум осям на пересечении x and y
 {
-    if (field[pos.y][pos.x].check_value > 0) return;
-    field[pos.y][pos.x].check_value = (field[pos.y][pos.x].check_value == BLACK_CELL ?
-        WHITE_CELL : BLACK_CELL);
+    if (field[pos.y][pos.x].check_value > 0) return ;
+    field[pos.y][pos.x].check_value = (field[pos.y][pos.x].check_value 
+        == BLACK_CELL ? WHITE_CELL : BLACK_CELL);
 
-    POS last = set_POS(0,0), cell = set_POS(0,0);
+    POSITION last = set_position(0,0), cell = set_position(0,0);
     for (int i = 0; i <= 7; i++)
     {
         if (field[pos.y][i].check_value == BLACK_CELL || i == 7)
@@ -154,41 +169,43 @@ _Bool check_axis(POS pos)//ГЇГ°Г®ГўГҐГ°ГЄГ  ГЇГ® Г¤ГўГіГ¬ Г®Г±ГїГ¬ Г­Г  ГЇГҐГ°ГҐ
             set_line(VERTIC, &last.y, i, &field[last.y][pos.x].free_cells_y);
     }
     return TRUE;
-
 }
 
-GAME_STATE draw_field(POS pos, HANDLE *c)
+
+GAME_STATE draw_field(POSITION pos)
 {
     _Bool trigger = TRUE;
+    GAME_STATE result = RUNNING;
     HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
-    printf("%s", line);
+
+    printf("%s", FIELD_LINE);
 
     for (int y = 0; y < 7; y++)
     {
         for (int x = 0; x < 7; x++)
         {
-            if (pos.x == x && pos.y == y) SetConsoleTextAttribute(console, 112);
+        if (pos.x == x && pos.y == y) SetConsoleTextAttribute(console, 112);
 
-            if (field[y][x].check_value > 0) 
-            {
-                if (field[y][x].free_cells_x +
-                    field[y][x].free_cells_y < field[y][x].check_value + 1)
-                    return LOSE;
+        if (field[y][x].check_value > 0)
+        {
+            if (field[y][x].free_cells_x +
+                field[y][x].free_cells_y < field[y][x].check_value + 1)
+                result = LOSE;
 
-                else if (field[y][x].free_cells_x + field[y][x].free_cells_y 
-                    != field[y][x].check_value + 1) 
-                    trigger = FALSE; 
+            else if (field[y][x].free_cells_x + field[y][x].free_cells_y
+                != field[y][x].check_value + 1)
+                trigger = FALSE;
 
-                printf("|%5d|", field[y][x].check_value);
-            }
-            else printf("|%5c|", field[y][x].check_value == WHITE_CELL ? ' ' : 'X');
-            SetConsoleTextAttribute(console, 7);
+            printf("|%5d|", field[y][x].check_value);
+        }
+        else printf("|%5c|", field[y][x].check_value == WHITE_CELL ? ' ' : 'X');
+        SetConsoleTextAttribute(console, 7);
 
 
         }
-        printf("%s", line);
-    }
-    
-    if (trigger) return WIN;
-    return RUNNING;
+        printf("%s", FIELD_LINE);
+    }   
+
+    if (trigger) result = WIN;
+    return result;
 }
